60e44c9a939e2198313b320296fee8dc048574e7
Lê Hiền Lương <75094831+luongic@users.noreply.github.com>
2022-07-19 16:14:40 +0700

    home slider
    add slider prômtions in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import items from '../data/promotions'
+import filterItems from './Menu'
 
 function Home() {
-    return <main>
-    <section className='menu section'>
-      <div className='title'>
-        <h2 className='brand'>Home</h2>
-        <div className='underline'></div>
-      </div>
-
-    </section>
-  </main>;
+    return <div className='home'>
+      <Slider/>
+    </div>
+    
+;
 }
 
+
+function Slider() {
+  const [index, setIndex] = useState(0)
+  useEffect(() => {
+    const lastIndex = items.length - 1;
+    if (index < 0){
+      setIndex(lastIndex)
+    } 
+    if (index > lastIndex){
+      setIndex(0)
+    } 
+  },[index])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 3000)
+    return () => clearInterval(slider )
+  }, [index])
+
+
+  return <div className="slider">
+
+    <div className='left-btn' onClick={() => setIndex(index - 1)}>
+      <i className="fa-solid fa-angle-left"></i>
+    </div>
+
+    {items.map((item, personIndex) => {
+      let positon = 'nextSlide'
+      if (personIndex === index){
+        positon = 'activeSlide'
+      }
+      if (personIndex === index - 1 || (index === 0 && personIndex === items.length - 1)){
+        positon = 'lastSlide'
+      }
+      const classes = `slide-item ${positon}`
+      return <div className={classes} key={item.id}>
+        <img src={item.img} alt={item.title} className="slide-photo" />
+        <button className='slide-btn'>
+          <Link to='/menu' filterItems = {'promotions'}>Mua ngay</Link>
+        </button>
+      </div>
+    })}
+
+    <div className='right-btn'onClick={() => setIndex(index + 1)} >
+      <i className="fa-solid fa-angle-right"></i>
+    </div>
+
+
+  </div>;
+}
+
+
+
+
 export default Home;
+
+
