9dd02a3fb92387e78fb962555670a8e8be39902d
Lê Hiền Lương <75094831+luongic@users.noreply.github.com>
2022-07-24 18:08:05 +0700

    cart update
    change order item quantity

--- src/components/Navcart.js
+++ src/components/Navcart.js
@@ -13,9 +13,11 @@ function Navcart() {
 
     let sum = 0;
 
-    bills.forEach(element => {
-        sum += element.price * element.quantity;
-    });
+    if (bills !== null){
+        bills.forEach(element => {
+            sum += element.price * element.quantity;
+        });
+    }
 
     function delFromcart(index){
         const delItem = bills.slice(0,index).concat(bills.slice(index+1))
@@ -23,18 +25,42 @@ function Navcart() {
         setBill(delItem)
     }
 
-
     const handleDel = index =>{
+
         delFromcart(index)
+    }
+
+    const handleUpdate = (method, index, title, size, crust) =>{
+        if (method === '+'){
+            const bill = bills.filter( bill => (bill.title === title && bill.size === size && bill.crust === crust))
+            bill[0].quantity += 1
+            
+            
+            localStorage.setItem('bill', JSON.stringify(bills))
+            setHover(!hover);
+        }
+        else {
+            const bill = bills.filter( bill => (bill.title === title && bill.size === size && bill.crust === crust))
+            bill[0].quantity -= 1
+
+            if (bill[0].quantity <= 0){
+                bill[0].quantity = 0
+                handleDel(index)
+                return
+            }
+            
+            localStorage.setItem('bill', JSON.stringify(bills))
+            setHover(!hover);
+        }
     }
 
 
     return <div className='navbar-cart'  onMouseOver={() => {
-        setHover(true);
-    }}
-    onMouseOut={() => {
-        setHover(false);
-    }}>
+                    setHover(!hover);
+                }}
+                onMouseOut={() => {
+                    setHover(!hover);
+                }}>
                 <div className='navbar-cart__text'>
                     <i className="fa-solid fa-cart-shopping"></i> Giỏ Hàng
                 </div>
@@ -44,6 +70,7 @@ function Navcart() {
 
             <ul className='navbar-cart__list'>
 
+                {bills.length === 0 ? <div className="navbar-cart__list-empty"><i className="fa-solid fa-face-frown"></i> bạn chưa chọn món nào</div> : <>
                 {bills.map((bill, index) => {
                     return <li key={index} className='navbar-cart__item'>
                     <div className='navbar-cart__item-content'>
@@ -52,10 +79,15 @@ function Navcart() {
 
                         <div className='navbar-cart__item-bottom'>
                             <div className='navbar-cart__item-selection'>
-                                <div className='navbar-cart__item-size'>{bill.size}</div>
+                                <div className='navbar-cart__item-size'>Size: {bill.size}</div>
                                 <div className='navbar-cart__item-crust'>{bill.crust}</div>
                             </div>
-                            <div className='navbar-cart__item-quantity'> <span>x {bill.quantity}</span> </div>
+                            <div className='navbar-cart__item-quantity'>
+                                <button className="navbar-cart__item-quantity-minus" disabled={bill.quantity === 1} 
+                                onClick={ () => handleUpdate('-', index, bill.title, bill.size, bill.crust, bill.quantity)} >-</button>
+                                <span>x {bill.quantity}</span> 
+                                <button className="navbar-cart__item-quantity-plus" onClick={ () => handleUpdate('+', index, bill.title, bill.size, bill.crust, bill.quantity)} >+</button>
+                            </div>
                             <div className='navbar-cart__item-price'>  <span>{(bill.quantity * bill.price).toLocaleString('en-US')},000 ₫</span> </div>
                         </div>
 
@@ -65,15 +97,19 @@ function Navcart() {
                     </div>
                 </li>;
                 })}
+                </>}
+
+                
 
             </ul>
-            <div className='navbar-cart__btn'>
-                <div className="navbar-cart__btn-text">
+            <div className='navbar-cart__btn'> {bills.length === 0 ? <>Hãy chọn món cho hôm nay</> : <><div className="navbar-cart__btn-text">
                     Đến trang thanh toán
                 </div>
                 <div className="navbar-cart__btn-text">
                     {sum.toLocaleString('en-US')},000₫
                 </div>
+            </>}
+                
             </div>
         </div>
     </div>
