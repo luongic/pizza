3a91429614f6868099f9a3f28ad612e043f17d07
Lê Hiền Lương <75094831+luongic@users.noreply.github.com>
2022-07-30 18:09:33 +0700

    update total after use voucher
    change value

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -139,7 +139,7 @@ function Bill() {
                                     </div>
                                     <div className="cart__total">
                                         <div className="cart__total-text">Tổng cộng:</div>
-                                        <div className="cart__total-number" >{context.voucher !== 0 ? (context.total*1000 + 22000).toLocaleString('it-IT', {style : 'currency', currency : 'VND'}) : 
+                                        <div className="cart__total-number" >{context.voucher !== 0 ? (context.total*1000).toLocaleString('it-IT', {style : 'currency', currency : 'VND'}) : 
                                         (sum*1000).toLocaleString('it-IT', {style : 'currency', currency : 'VND'})
                                         }</div>
                                     </div>
