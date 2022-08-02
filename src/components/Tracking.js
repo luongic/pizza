b567291ba37d62ae9c12e3444617a66b5587d5f9
Lê Hiền Lương <75094831+luongic@users.noreply.github.com>
2022-08-02 16:08:11 +0700

    add tracking ui
    tracking header UI

--- src/components/Tracking.js
+++ src/components/Tracking.js
@@ -7,9 +7,23 @@ function Tracking() {
 
   console.log(Object.keys(params).length === 0);
 
-  return <div className='tracking'>
+  return  <div className='tracking'>
+            <div className='tracking__heading'>Kiểm tra đơn hàng</div>
+            <div className='tracking__contain'>
+            
+              <div className='tracking__contain-check'>
 
-  </div>
+                <div className='tracking__contain-input'>
+                  <div className='tracking__contain-input-label'>Nhập mã đơn hàng: </div>
+                  <input type='text' />
+                </div>
+                <div className='tracking__contain-btn'>
+                  Kiểm tra
+                </div>
+              </div>
+              <div className='tracking__contain-display'></div>
+            </div>
+          </div>
 }
 
 export default Tracking;
